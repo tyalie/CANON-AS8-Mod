0e238fc9f4f2789971f90193fe80d7259eaf9c74
Sophie 'Tyalie' Friedrich <dev@flowerpot.me>
2023-11-02 14:53:10 +0100

    feat: canon-as8: Finish key-matrix in overlay
    This patch adds the complete key-matrix in the overlay file as defined
    by my current hardware.
    
    In order to represent all keys, additional keycodes have been added.
    
    Signed-off-by: Sophie 'Tyalie' Friedrich <dev@flowerpot.me>

---
+++ include/os/input/keycodes.h
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2023 Sophie Tyalie
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+#define INPUT_KEY_CEQ 0xff00              // calc key =
+#define INPUT_KEY_CPLUS 0xff01            // calc key +
+#define INPUT_KEY_CMINUS 0xff02           // calc key -
+#define INPUT_KEY_CMUL 0xff03             // calc key ×
+#define INPUT_KEY_CDIV 0xff04             // calc key ÷
+#define INPUT_KEY_CSQRT 0xff05            // calc key √
+#define INPUT_KEY_CPCTPM 0xff06           // calc key %±
+#define INPUT_KEY_CCI 0xff07              // calc key CI
+#define INPUT_KEY_CCON 0xff08             // calc key C/ON
+#define INPUT_KEY_CRMCM 0xff09            // calc key RM/CM
+#define INPUT_KEY_CMPLUS 0xff0a           // calc key M+/=
+#define INPUT_KEY_CMMINUS 0xff0b          // calc key M-/=
